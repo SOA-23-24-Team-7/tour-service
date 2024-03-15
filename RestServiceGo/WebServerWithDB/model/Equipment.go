0f2b2df55259837dd387175e5688973cb4fb92c8
Jelena Kovač <jelena.kovac765@gmail.com>
2024-03-15 16:53:59 +0100

    feat: equipment creation and view

---
+++ RestServiceGo/WebServerWithDB/model/Equipment.go
@@ -0,0 +1,19 @@
+package model
+
+import "errors"
+
+type Equipment struct {
+	Id          int64  `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+func (equipment *Equipment) Validate() error {
+	if equipment.Name == "" || equipment.Name == " " {
+		return errors.New("name can not be empty")
+	}
+	if equipment.Description == "" || equipment.Description == " "{
+		return errors.New("descritprion can not be empty")
+	}
+	return nil
+}
